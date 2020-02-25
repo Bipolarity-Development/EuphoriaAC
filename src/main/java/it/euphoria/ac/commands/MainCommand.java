7f352ef97bf28f5fd1d0e4e863efc82c4eabad6c
Mr_L4ST <globyt75@gmail.com>
2020-02-25 11:07:12 +0100

    Added Info Command

--- src/main/java/it/euphoria/ac/commands/MainCommand.java
+++ src/main/java/it/euphoria/ac/commands/MainCommand.java
@@ -18,13 +18,26 @@ public class MainCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (player.hasPermission("euphoria.admin")) {
-                player.sendMessage("§b§m--------------------");
-                player.sendMessage("");
-                player.sendMessage("§e/euphoria reload");
-                player.sendMessage("§e/euphoria info");
-                player.sendMessage("");
-                player.sendMessage("§b§m--------------------");
+            if (args.length == 0 ) {
+                if (player.hasPermission("euphoria.admin")) {
+                    player.sendMessage("§b§m--------------------");
+                    player.sendMessage("");
+                    player.sendMessage("§e/euphoria reload");
+                    player.sendMessage("§e/euphoria info");
+                    player.sendMessage("");
+                    player.sendMessage("§b§m--------------------");
+                }
+            } else {
+                if (args.length == 1) {
+                    if (args[0].equalsIgnoreCase("info")){
+                        player.sendMessage("§b§m--------------------");
+                        player.sendMessage("");
+                        player.sendMessage("§cUntitlxd_ §b§l& §cMr_L4ST");
+                        player.sendMessage("");
+                        player.sendMessage("§b§m--------------------");
+
+                    }
+                }
             }
         } else {
             sender.sendMessage("§7You must be a player to execute this command.");
